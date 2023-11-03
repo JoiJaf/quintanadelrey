cd4a4560df84c762d5732d7e7b14f2748a3367f0
DrexGuzman <drexjesusguzman@gmail.com>
2023-11-02 22:18:15 -0600

    database conection file and medoo

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php 
+    namespace Medoo;
+    require 'Medoo.php';
+    
+    /* 
+    - For Laragon: username='root' / password=''
+    - For MAMP: username='root' / password='root'
+      */
+    $database = new Medoo([
+        'type'=>'mysql',
+        'host' => 'localhost',
+        'database' => 'quintanadelrey',
+        'username' => 'root',
+        'password' => ''
+    ]);
+
+
+?>
