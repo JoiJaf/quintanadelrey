b67acfcc5a4ac5441b5d48be15c073bcfa81845d
DrexGuzman <drexjesusguzman@gmail.com>
2023-11-15 21:37:09 -0600

    register new user fixed with logic to verify if the ID, email or user name is not avaible, also the login is working redirect to the index with the user name at nav bar and can logout.

--- response.php
+++ response.php
@@ -2,20 +2,24 @@
     require_once 'database.php';
     // Reference: https://medoo.in/api/insert
     if($_POST){
-    $database->insert("tb_usuarios",[
-        "nombre"=> $_POST["firstName"],
-        "apellido"=> $_POST["lastName"],
-        "cedula"=> $_POST["id"],
-        "telefono"=> $_POST["phone"],
-        "correo"=> $_POST["email"],
-        "fecha_nacimiento"=> $_POST["birthday"],
-        "nombre_usuario"=> $_POST["user"],
-        "contrasena"=> $_POST["password"]
-    ]);
-    header("location: index.php");
+        
+    // $pass = password_hash($_POST["password"], PASSWORD_DEFAULT, ['cost' => 12]);//revisar
+    // $database->insert("tb_usuarios",[
+    //     "nombre"=> $_POST["firstName"],
+    //     "apellido"=> $_POST["lastName"],
+    //     "cedula"=> $_POST["id"],
+    //     "telefono"=> $_POST["phone"],
+    //     "correo"=> $_POST["email"],
+    //     "fecha_nacimiento"=> $_POST["birthday"],
+    //     "nombre_usuario"=> $_POST["user"],
+    //     "contrasena"=> $pass
+    // ]);
+    //header("location: index.php");
     }
     if($_GET){
-
+        $newUser = $database->select("tb_usuarios","*",[
+            "id_usuario"=>$_GET["id"]
+        ]);
     }
 ?>
 
@@ -27,10 +31,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Gracias por registrarte en nuestro sitio web</h1>
+    <h1>Thanks for register on our website</h1>
     <?php
-    echo $_POST["firstName"];
+    echo $newUser[0]["nombre"]."<br>";
     ?>
+    <div>
+    <a href="register.php?="<?php echo $newUser[0]["id_usuario"];?>>Login</a>
+    </div>
+    <a href="index.php">Return to main page</a>
     <p></p>
 </body>
 </html>
